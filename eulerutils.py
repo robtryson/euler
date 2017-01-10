7aaf09abee219de0c509511706db664c92d821e4
Robert Tryson <robert.tryson@its.ny.gov>
2017-01-10 11:24:43 -0500

    Added prime_list_ind - good for high maxval prime list

--- eulerutils.py
+++ eulerutils.py
@@ -24,12 +24,27 @@ def prime_list(maxval):
                 pl[check] = 0
                 check += step
         i += 1
-
+    print(pl)
     b = list()
     for k, v in enumerate(pl):
         if v == 1:
             b.append(k)
     return b
+    
+def prime_list_ind(maxval):
+    pl = [1 for maxval in range(0,maxval+1)]
+    pl[0] = 0
+    pl[1] = 0
+    i = 2
+    while i <= math.sqrt(maxval):
+        if pl[i] == 1:
+            step = i
+            check = 2*i
+            while check <= maxval:
+                pl[check] = 0
+                check += step
+        i += 1
+    return pl
 	
 def primelist(minval, maxval):
     # pl initially all 1s
